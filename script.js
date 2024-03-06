2568768dde9d1f39969b5aa4bac2c3e7748a7e1c
fernandasss <fernandasss@gmail.com>
2024-03-06 10:24:29 -0300

    script.js
    funciones y variables del encriptador.

---
+++ script.js
@@ -0,0 +1,46 @@
+let texto = document.querySelector(".textoaEncriptar");
+let texto_encriptado = document.querySelector(".textoEncriptado");
+
+let codigoMatriz = [
+    ["e", "enter"],
+    ["i", "imes"],
+    ["a", "ai"],
+    ["o", "ober"],
+    ["u", "ufat"],
+];
+function btnEncriptar (){
+    let textoEncriptar = encriptar(texto.value);
+    texto_encriptado.value = textoEncriptar;
+}
+function encriptar (fraseEncriptada){
+    fraseEncriptada=fraseEncriptada.toLowerCase()
+    for (let i=0; i < codigoMatriz.length; i++){
+        if(fraseEncriptada.includes (codigoMatriz[i][0])){
+            fraseEncriptada=fraseEncriptada.replaceAll(
+                codigoMatriz [i][0],
+                codigoMatriz [i][1])
+        }
+    }
+    return fraseEncriptada
+} 
+function btnDesencriptar (){
+    let textoEncriptar = desencriptar(texto.value);
+    texto_encriptado.value = textoEncriptar;
+}
+
+function desencriptar (fraseDesencriptada){
+    for (let i=0; i < codigoMatriz.length; i++){
+        if(fraseDesencriptada.includes (codigoMatriz[i][1])){
+            fraseDesencriptada=fraseDesencriptada.replaceAll(
+                codigoMatriz [i][1],
+                codigoMatriz [i][0])
+        }
+    }
+    return fraseDesencriptada
+}
+function copiar(){
+    texto_encriptado.select();
+    navigator.clipboard.writeText(texto_encriptado.value)
+    texto_encriptado.value = "";
+    alert("Texto copiado");
+}
